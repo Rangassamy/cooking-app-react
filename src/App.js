52a41124b7dc84f962670e7383d0371b5cdbcc1d
Rangassamy <enzorangassamy@gmail.com>
2024-08-04 21:09:46 -0400

    first version cooking app react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./styles/app.css";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+  const [query, setQuery] = useState("beef"); // Valeur initiale de la requête
+
+  useEffect(() => {
+    // Supprimer l'état de chargement
+    axios
+      .get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data.meals);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, [query]); // Dépendance au texte de la requête
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value); // Met à jour la requête en fonction de l'entrée
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Cooking App in React!</h1>
+      <input
+        type="text"
+        value={query}
+        onChange={handleInputChange}
+        placeholder="Search for a meal..."
+      />
+      <ul className="meal-container">
+        {data && data.length > 0 ? (
+          data.map((meal) => (
+            <li key={meal.idMeal} className="meal-card">
+              <h2 className="meal-title">{meal.strMeal}</h2>
+              <img
+                src={meal.strMealThumb}
+                alt={meal.strMeal}
+                className="meal-image"
+              />
+              <a
+                href={meal.strSource}
+                className="meal-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Recipe Source
+              </a>
+            </li>
+          ))
+        ) : (
+          <div>No results found</div>
+        )}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
